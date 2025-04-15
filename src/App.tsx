51d56483da66a70744a88ebf64cb171054b2b87a
YutaYamada <y-yamada@techqiao.co.jp>
2025-04-15 15:21:56 +0900

    feat: Appコンポーネントにルーティングを追加し、AppRoutesを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
 import MainLayout from './layouts/MainLayout'
-import AppRoutes from './routes/AppRoutes'
 import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+
 export default function App() {
   return (
     <BrowserRouter>
       <MainLayout>
-        <AppRoutes />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
       </MainLayout>
     </BrowserRouter>
   );
