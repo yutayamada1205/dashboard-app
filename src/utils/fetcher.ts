f2b7cc3d8c7468e2cb99b287011ffa7438273953
YutaYamada <y-yamada@techqiao.co.jp>
2025-04-16 16:24:35 +0900

    feat: fetcher関数の型定義を削除

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,7 +1,7 @@
-export async function fetcher<T>(key: string): Promise<T> {
+export async function fetcher(key: string) {
   const res = await fetch(key)
   if (!res.ok) {
     throw new Error("ネットワークエラーが発生しました")
   }
-  return res.json() as Promise<T>
+  return res.json()
 }
