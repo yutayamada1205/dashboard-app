321c8b5e10379a19a66a4fab022008b6b18b92f2
YutaYamada <y-yamada@techqiao.co.jp>
2025-04-18 10:54:55 +0900

    feat: ヘッダーコンポーネントを追加し、ログアウト機能を実装

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,18 @@
 import Sidebar from '../components/Sidebar'
 import MainContent from '../components/MainContent'
+import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
 
 export default function MainLayout(){
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <MainContent>
-        <Outlet />
-      </MainContent>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <MainContent>
+          <Outlet />
+        </MainContent>
+      </div>
     </div>
   )
 }
